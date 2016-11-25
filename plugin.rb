4191cdc247f4e9051a8aae8ee5784bb9587ff451
cpradio <forums@cpradio.com>
2016-11-25 15:41:27 -0500

    Use d-modal-body

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-plugin-imgflip
 # about: Add ImgFlip support
-# version: 1.1.0
+# version: 1.1.1
 # authors: Matthew Wilkin
 # url: https://github.com/cpradio/discourse-plugin-imgflip
 
